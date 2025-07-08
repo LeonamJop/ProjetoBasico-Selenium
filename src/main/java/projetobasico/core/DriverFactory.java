09fd8dab21dcb57f28309e5f75c4a026a7dc4a28
Leonam Silva <leonam.murilo@gmail.com>
2025-07-08 11:04:32 -0300

    Test: Um driver por thread

--- src/main/java/projetobasico/core/DriverFactory.java
+++ src/main/java/projetobasico/core/DriverFactory.java
@@ -9,32 +9,50 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverFactory {
 	
-	private static WebDriver driver;
+	//private static WebDriver driver;
+	
+	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>() {
+		@Override	
+		protected synchronized WebDriver initialValue() {
+			return initDriver();
+		}
+	};
 	
 	private DriverFactory() {}
 	
 	public static WebDriver getDriver() {
-		if(driver == null) {
+		return threadDriver.get();
+	}
+	
+	public static WebDriver initDriver() {
+		WebDriver driver = null;
+		
+		switch (Propriedades.browser) {
+			case EDGE: driver = new EdgeDriver();
+			break;
 			
-			switch (Propriedades.browser) {
-				case EDGE: driver = new EdgeDriver();
-				break;
-				
-				case CHROME: driver = new ChromeDriver();
-				break;
-			}
-			
-			driver.manage().window().setSize(new Dimension(1200, 765));
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+			case CHROME: driver = new ChromeDriver();
+			break;
+		default:
+			break;
 		}
+		
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
 		return driver;
 	}
 	
 	public static void killDriver() {
+		WebDriver driver = getDriver();
+		
 		if(driver != null) {
 			driver.quit();
 			driver = null;
+		}
+		
+		if(threadDriver != null) {
+			threadDriver.remove();
 		}
 	}
 
